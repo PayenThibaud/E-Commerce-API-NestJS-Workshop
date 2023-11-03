9c3ee3619521694ae0f5d37c48e1139a0ef2ea67
Mecopi <mecopilive@gmail.com>
2023-11-03 23:04:28 +0100

    feat: add route to get products name table

--- e-commerce/src/product.controller.ts
+++ e-commerce/src/product.controller.ts
@@ -1,6 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ProductService } from './product.service';
 
-@Controller()
+@Controller('products')
 export class ProductController {
-  constructor() {}
+  constructor(private readonly productService: ProductService) {}
+
+  @Get()
+  getProducts(): string[] {
+    return this.productService.getProducts();
+  }
 }
--- e-commerce/src/product.service.ts
+++ e-commerce/src/product.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  getProducts(): string[] {
+    return ['Moto', 'Vélo', 'Voiture'];
+  }
+}
