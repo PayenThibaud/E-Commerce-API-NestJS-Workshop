dae23c5a2e4754806ba5e9d34670ac7db869f172
Mecopi <mecopilive@gmail.com>
2023-11-03 22:35:50 +0100

    docs: add product controller

--- e-commerce/src/product.controller.ts
+++ e-commerce/src/product.controller.ts
@@ -1,6 +1,6 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
-@Controller()
+@Controller('products')
 export class ProductController {
   constructor() {}
 }
