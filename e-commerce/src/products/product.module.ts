84515deb5a4d21b32db6e28dc76710eb50a1d73a
Mecopi <mecopilive@gmail.com>
2023-11-04 02:32:02 +0100

    feat: link

--- e-commerce/src/products/product.module.ts
+++ e-commerce/src/products/product.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ProductController } from './product.controller';
+import { ProductService } from './product.service';
 
 @Module({
   imports: [],
-  controllers: [],
-  providers: [],
+  controllers: [ProductController],
+  providers: [ProductService],
 })
 export class ProductModule {}
