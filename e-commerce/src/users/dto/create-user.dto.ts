9f03862328eb4b99e3fb6e99d5112798149fc162
Mecopi <mecopilive@gmail.com>
2023-11-05 04:26:38 +0100

    docs(Swagger): add decorators to DTO to explain their usages

--- e-commerce/src/users/dto/create-user.dto.ts
+++ e-commerce/src/users/dto/create-user.dto.ts
@@ -1,10 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, Length } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'This field represents the new user nickname',
+    minLength: 5,
+    maxLength: 50,
+  })
   @IsString()
   @Length(5, 50)
   pseudo: string;
 
+  @ApiProperty({
+    description: 'This field represents the new user mail address',
+    minLength: 2,
+    maxLength: 75,
+    example: 'mymail@gmail.com',
+  })
   @IsEmail()
   @Length(2, 75)
   mail: string;
