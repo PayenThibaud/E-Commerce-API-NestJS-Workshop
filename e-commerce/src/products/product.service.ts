0c15b64b451457f7e4b83376a30a0649d56c5a2c
Mecopi <mecopilive@gmail.com>
2023-11-05 07:57:50 +0100

    feat: add logic for creating new product & import prisma service

--- e-commerce/src/products/product.service.ts
+++ e-commerce/src/products/product.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import NormalizedResponse from 'src/utils/normalized-response';
 
 @Injectable()
 export class ProductService {
+  constructor(private readonly prisma: PrismaService) {}
+
   getProducts(): string[] {
     return ['Moto', 'Vélo', 'Voiture'];
+  }
+
+  public async create(createProductDto: CreateProductDto) {
+    return new NormalizedResponse(
+      `Product ${createProductDto.name} has been registered`,
+      await this.prisma.products.create({
+        data: {
+          Name: createProductDto.name,
+          Description: createProductDto.description,
+          Price: createProductDto.price,
+          Author: {
+            connect: {
+              UUID: createProductDto.owner_uuid,
+            },
+          },
+        },
+      }),
+    ).toJSON();
   }
 }
