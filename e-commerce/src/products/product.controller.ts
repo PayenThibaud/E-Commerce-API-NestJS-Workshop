f51c95fbd4ea98a0c82b1289b61a83022cf84527
Mecopi <mecopilive@gmail.com>
2023-11-05 07:23:55 +0100

    feat: add route to create new product

--- e-commerce/src/products/product.controller.ts
+++ e-commerce/src/products/product.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ProductService } from './product.service';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Controller('products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+
+  @Post()
+  public create(@Body() createProductDto: CreateProductDto) {
+    return this.productService.create(createProductDto);
+  }
 
   @Get()
   getProducts(): string[] {
