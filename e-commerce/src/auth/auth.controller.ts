7af768d6c1e350a1667e275631a7d2ea8ad81f3d
Mecopi <mecopilive@gmail.com>
2023-11-06 13:18:11 +0100

    feat: add decorator to say no auth required to logi

--- e-commerce/src/auth/auth.controller.ts
+++ e-commerce/src/auth/auth.controller.ts
@@ -1,11 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, SetMetadata } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginUserDto } from './dto/login-user.dto';
+
+export const IS_PUBLIC_KEY = 'isPublic';
+export const AuthNotRequired = () => SetMetadata(IS_PUBLIC_KEY, true);
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @AuthNotRequired()
   @Post()
   public login(@Body() loginUserDto: LoginUserDto) {
     return this.authService.login(loginUserDto);
