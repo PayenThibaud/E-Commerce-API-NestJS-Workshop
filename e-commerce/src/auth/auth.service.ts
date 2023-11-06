62a35b55178790d7a47d78f630527e7bb1b80e3e
Mecopi <mecopilive@gmail.com>
2023-11-06 13:37:57 +0100

    feat: add logic to login user and deliver JWT

--- e-commerce/src/auth/auth.service.ts
+++ e-commerce/src/auth/auth.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { LoginUserDto } from './dto/login-user.dto';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly jwt: JwtService,
+    private readonly users: UsersService,
+  ) {}
+
+  public async login(loginUserDto: LoginUserDto) {
+    this.validateCredentials(loginUserDto.username, loginUserDto.password);
+    return this.buildJWT(loginUserDto);
+  }
+
+  public async validateCredentials(username: string, password: string) {
+    const targetedUser = await this.users.getByUsername(username);
+
+    if (
+      targetedUser === null ||
+      !(await bcrypt.compare(password, targetedUser.data.password))
+    ) {
+      throw new UnauthorizedException();
+    }
+
+    return targetedUser;
+  }
+
+  private buildJWT(user: any) {
+    const jwt_payload = {
+      username: user.username,
+      uuid: user.UUID,
+    };
+    return { access_token: this.jwt.sign(jwt_payload) };
+  }
+}
