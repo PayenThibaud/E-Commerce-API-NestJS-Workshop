b96b5407f5d6ae3e048cb02e66874c5f62583864
Mecopi <mecopilive@gmail.com>
2023-11-07 00:38:01 +0100

    fix: remove 'JwtService' from providers

--- e-commerce/src/auth/auth.module.ts
+++ e-commerce/src/auth/auth.module.ts
@@ -1,20 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
-<<<<<<< HEAD
+import { ProductService } from 'src/products/product.service';
 import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [PrismaService, AuthService, JwtService, UsersService],
-=======
-import { ProductService } from 'src/products/product.service';
-
-@Module({
-  controllers: [AuthController],
-  providers: [AuthService, JwtService, ProductService, UsersService],
->>>>>>> 97f1edd (fix: import product service as provider)
+  providers: [PrismaService, AuthService, ProductService, UsersService],
 })
 export class AuthModule {}
