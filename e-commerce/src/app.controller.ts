1bd67e77cb997bc123b5275deccb3271af49c130
Mecopi <mecopilive@gmail.com>
2023-11-06 02:20:08 +0100

    refactor: rm garbage funcs

--- e-commerce/src/app.controller.ts
+++ e-commerce/src/app.controller.ts
@@ -9,9 +9,4 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-
-  @Get('direBonjour')
-  direBonjour() {
-    return this.appService.logiqueDeDireBonjour();
-  }
 }
