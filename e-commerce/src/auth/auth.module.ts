3aa539b995f3b3a6ec35d652b2c760c4786f1ca2
Mecopi <mecopilive@gmail.com>
2023-11-06 13:57:52 +0100

    fix conflict

--- e-commerce/src/auth/auth.module.ts
+++ e-commerce/src/auth/auth.module.ts
@@ -3,10 +3,18 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
+<<<<<<< HEAD
 import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [AuthController],
   providers: [PrismaService, AuthService, JwtService, UsersService],
+=======
+import { ProductService } from 'src/products/product.service';
+
+@Module({
+  controllers: [AuthController],
+  providers: [AuthService, JwtService, ProductService, UsersService],
+>>>>>>> 97f1edd (fix: import product service as provider)
 })
 export class AuthModule {}
