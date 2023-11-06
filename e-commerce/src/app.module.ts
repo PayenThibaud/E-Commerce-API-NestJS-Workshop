36ae1ab362a49e2e40e3bead2cdc4215c7bf27bc
Mecopi <mecopilive@gmail.com>
2023-11-06 05:13:19 +0100

    feat: add auth module

--- e-commerce/src/app.module.ts
+++ e-commerce/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './products/product.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ProductModule, UsersModule],
+  imports: [ProductModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
