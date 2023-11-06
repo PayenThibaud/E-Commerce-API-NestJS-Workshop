9aad13a5d84ec5d9ee91498dbdee7a2724e00798
Mecopi <mecopilive@gmail.com>
2023-11-07 00:39:35 +0100

    fix: rebase

--- e-commerce/src/auth/auth.module.ts
+++ e-commerce/src/auth/auth.module.ts
@@ -1,12 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 import { ProductService } from 'src/products/product.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategies/jwt.strategy';
 import { PrismaService } from 'src/prisma.service';
 
 @Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [PrismaService, AuthService, ProductService, UsersService],
+  providers: [
+    PrismaService,
+    AuthService,
+    ProductService,
+    UsersService,
+    JwtStrategy,
+  ],
 })
 export class AuthModule {}
